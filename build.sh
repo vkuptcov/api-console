5963289b7f08b177095ddf7697df77a9c9fee9f7
Pawel Psztyc <jarrodek@gmail.com>
2017-05-25 18:12:06 +0100

    Updated the build script to build the API that works with new builder

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-api-console build https://cdn.rawgit.com/advanced-rest-client/raml-example-api/d8b474ff/api.raml --json --inline-json --verbose
+api-console build https://cdn.rawgit.com/advanced-rest-client/drive-raml-api-v2/1f85d308/api.raml --json --inline-json --verbose
 cp -R build/. .
 rm -rf build/
