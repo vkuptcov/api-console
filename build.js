ba6c95f1592fecc39dc017f992353cc3e9527a05
Pawel Psztyc <jarrodek@gmail.com>
2017-06-15 17:54:07 +0100

    Small changes in the build script for node module. CLI is still preffered way to build the console.

--- build.js
+++ build.js
@@ -2,11 +2,11 @@ const builder = require('api-console-builder');
 
 builder({
   src: 'https://github.com/mulesoft/api-console/archive/release/4.0.0.zip',
-  // dest: 'build',
+  dest: 'build',
   raml: 'https://cdn.rawgit.com/advanced-rest-client/drive-raml-api-v2/1f85d308/api.raml',
-  // verbose: true,
-  // useJson: true,
-  // inlineJson: true,
+  verbose: true,
+  useJson: true,
+  inlineJson: true,
   noOptimization: true
 })
 .then(() => console.log('Build complete'))
